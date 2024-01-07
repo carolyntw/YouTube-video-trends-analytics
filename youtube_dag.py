270965df8d5d9b1083248ccecd443f0bfd13efb3
carolyntw <carolyn961208@gmail.com>
2024-01-06 23:05:16 -0800

    add process_data_task

--- youtube_dag.py
+++ youtube_dag.py
@@ -2,12 +2,13 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from scrapper import run_youtube_etl
+from data_processing import process_data
 
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 12, 20),
+    'start_date': datetime(2023, 12, 28),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
@@ -15,7 +16,7 @@ default_args = {
 dag = DAG(
     'youtube_etl_dag',
     default_args=default_args,
-    description='A simple tutorial DAG for YouTube ETL',
+    description='A DAG for YouTube ETL',
 )
 
 run_etl = PythonOperator(
@@ -24,4 +25,10 @@ run_etl = PythonOperator(
     dag=dag,
 )
 
-run_etl
+process_data_task = PythonOperator(
+    task_id='process_youtube_data',
+    python_callable=process_data,
+    dag=dag,
+)
+
+run_etl >> process_data_task
