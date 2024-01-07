ca5a80b45763e24ec8ee09f73ee213cc05175fc0
carolyntw <carolyn961208@gmail.com>
2024-01-06 23:10:49 -0800

    update format

--- youtube_dag.py
+++ youtube_dag.py
@@ -6,27 +6,27 @@ from data_processing import process_data
 
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 12, 28),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 12, 28),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    'youtube_etl_dag',
+    "youtube_etl_dag",
     default_args=default_args,
-    description='A DAG for YouTube ETL',
+    description="A DAG for YouTube ETL",
 )
 
 run_etl = PythonOperator(
-    task_id='run_youtube_etl',
+    task_id="run_youtube_etl",
     python_callable=run_youtube_etl,
     dag=dag,
 )
 
 process_data_task = PythonOperator(
-    task_id='process_youtube_data',
+    task_id="process_youtube_data",
     python_callable=process_data,
     dag=dag,
 )
